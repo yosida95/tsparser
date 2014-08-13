080f858120c4b2412347a72880986e510a0707a5
yosida95 <kohei@yosida95.com>
2014-08-14 03:12:56 +0900

    impl PacketFilter

--- tsparser/filter.go
+++ tsparser/filter.go
@@ -1,104 +1,39 @@
 package tsparser
 
-type filterBuffer struct {
-	ok     bool
-	data   []byte
-	length int
+type PacketFilter struct {
+	s    PacketStream
+	pids []PID
+	next Packet
 }
 
-func (b *filterBuffer) extendData(right []byte) {
-	left := b.data
-	b.data = make([]byte, len(left)+len(right))
-	copy(b.data, left)
-	copy(b.data[len(left):], right)
-}
-
-func (b *filterBuffer) Extend(packet Packet) {
-	isFirst := len(b.data) == 0
-
-	payload := packet.Payload()
-	if !packet.payloadUnitStartIndicator() {
-		if !isFirst {
-			b.extendData(payload)
-		}
-		b.ok = false
-		return
-	}
-
-	if !isFirst {
-		b.data = b.data[b.length:]
-		b.length = len(b.data)
-		b.ok = true
-	}
-	packetStartCodePrefix := payload[0]<<16 | payload[1]<<8 | payload[2]
-	if packetStartCodePrefix == 0x000001 {
-		// Packetized Elementary Stream
-		b.extendData(payload)
-	} else {
-		// Program Specific Information
-		pointerField := int(payload[0])
-		if isFirst {
-			b.extendData(payload[1+pointerField:])
-		} else {
-			b.length += pointerField
-			b.extendData(payload[1:])
-		}
-	}
-
-	return
-}
-
-func (b *filterBuffer) OK() bool {
-	return b.ok
-}
-
-func (b *filterBuffer) Bytes() []byte {
-	if b.OK() {
-		return b.data[b.length:]
-	}
-
-	return nil
-}
-
-type Filter struct {
-	s       *Scanner
-	pid     PID
-	buffers map[PID]*filterBuffer
-}
-
-func NewFilter(scanner *Scanner, pids ...PID) *Filter {
-	buffers := make(map[PID]*filterBuffer)
-	for _, pid := range pids {
-		buffers[pid] = new(filterBuffer)
-	}
-
-	return &Filter{
-		s:       scanner,
-		buffers: buffers,
+func NewPacketFilter(s PacketStream, pids ...PID) *PacketFilter {
+	return &PacketFilter{
+		s:    s,
+		pids: pids,
 	}
 }
 
-func (f *Filter) Scan() bool {
+func (f *PacketFilter) isTarget(pid PID) bool {
+	return pid == 0x00 || pid == 0x14
+	return false
+}
+
+func (f *PacketFilter) Scan() bool {
 	for f.s.Scan() {
 		packet := f.s.Packet()
-
-		if buffer, ok := f.buffers[packet.PID()]; ok {
-			buffer.Extend(packet)
-			if buffer.OK() {
-				f.pid = packet.PID()
-				return true
-			}
+		if f.isTarget(packet.PID()) {
+			f.next = packet
+			return true
 		}
 	}
 
 	return false
 }
 
-func (f *Filter) Bytes() []byte {
-	buffer := f.buffers[f.pid]
-	if buffer.OK() {
-		return buffer.Bytes()
-	}
+func (f *PacketFilter) Bytes() []byte {
+	return []byte(f.next)
+}
 
-	return nil
+func (f *PacketFilter) Packet() Packet {
+	return f.next
 }
