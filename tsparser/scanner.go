07767c5cbba226ef71eefeccdb38634e83dd55e4
yosida95 <kohei@yosida95.com>
2014-08-14 03:13:49 +0900

    s/Scanner/PacketScanner/g, impl TableScanner

--- tsparser/scanner.go
+++ tsparser/scanner.go
@@ -12,7 +12,7 @@ const (
 	SyncByte byte = 0x47
 )
 
-type Scanner struct {
+type PacketScanner struct {
 	r      io.Reader
 	buffer [PacketSize * BufferedPacketCount]byte
 	seek   int
@@ -20,8 +20,8 @@ type Scanner struct {
 	err    error
 }
 
-func NewScanner(r io.Reader) *Scanner {
-	return &Scanner{
+func NewPacketScanner(r io.Reader) *PacketScanner {
+	return &PacketScanner{
 		r:    r,
 		seek: 0,
 		eof:  false,
@@ -29,21 +29,21 @@ func NewScanner(r io.Reader) *Scanner {
 	}
 }
 
-func (s *Scanner) Err() error {
+func (s *PacketScanner) Err() error {
 	return s.err
 }
 
-func (s *Scanner) leftShift(n int) {
+func (s *PacketScanner) leftShift(n int) {
 	copy(s.buffer[:BufferSize-n], s.buffer[n:])
 	s.seek -= n
 }
 
-func (s *Scanner) rightShift(n int) {
+func (s *PacketScanner) rightShift(n int) {
 	copy(s.buffer[n:], s.buffer[:BufferSize-n])
 	s.seek += n
 }
 
-func (s *Scanner) fillBuffer(n int) bool {
+func (s *PacketScanner) fillBuffer(n int) bool {
 	switch m, err := io.ReadFull(s.r, s.buffer[BufferSize-n:]); err {
 	case io.ErrUnexpectedEOF, io.EOF:
 		s.eof = true
@@ -59,7 +59,7 @@ func (s *Scanner) fillBuffer(n int) bool {
 	}
 }
 
-func (s *Scanner) isSynced() bool {
+func (s *PacketScanner) isSynced() bool {
 	synced := true
 	for i := s.seek; i < BufferSize; i += PacketSize {
 		if s.buffer[i] != SyncByte {
@@ -71,7 +71,7 @@ func (s *Scanner) isSynced() bool {
 	return synced
 }
 
-func (s *Scanner) sync() bool {
+func (s *PacketScanner) sync() bool {
 	for i := 0; i < PacketSize; i++ {
 		if !s.isSynced() {
 			s.seek++
@@ -89,7 +89,7 @@ func (s *Scanner) sync() bool {
 	return false
 }
 
-func (s *Scanner) Scan() bool {
+func (s *PacketScanner) Scan() bool {
 	if s.seek >= BufferSize {
 		return false
 	}
@@ -116,7 +116,7 @@ func (s *Scanner) Scan() bool {
 	return true
 }
 
-func (s *Scanner) Bytes() []byte {
+func (s *PacketScanner) Bytes() []byte {
 	if s.seek < BufferSize {
 		return s.buffer[s.seek : s.seek+PacketSize]
 	}
@@ -124,6 +124,113 @@ func (s *Scanner) Bytes() []byte {
 	return nil
 }
 
-func (s *Scanner) Packet() Packet {
+func (s *PacketScanner) Packet() Packet {
 	return Packet(s.Bytes())
+}
+
+type tableScannerBuffer struct {
+	ok     bool
+	data   []byte
+	length int
+}
+
+func (b *tableScannerBuffer) extendData(right []byte) {
+	left := b.data
+	b.data = make([]byte, len(left)+len(right))
+	copy(b.data, left)
+	copy(b.data[len(left):], right)
+}
+
+func (b *tableScannerBuffer) Extend(packet Packet) {
+	isFirst := len(b.data) == 0
+
+	payload := packet.Payload()
+	if !packet.payloadUnitStartIndicator() {
+		if !isFirst {
+			b.extendData(payload)
+		}
+		b.ok = false
+		return
+	}
+
+	if !isFirst {
+		b.data = b.data[b.length:]
+		b.length = len(b.data)
+		b.ok = true
+	}
+	packetStartCodePrefix := payload[0]<<16 | payload[1]<<8 | payload[2]
+	if packetStartCodePrefix == 0x000001 {
+		// Packetized Elementary Stream
+		b.extendData(payload)
+	} else {
+		// Program Specific Information
+		pointerField := int(payload[0])
+		if isFirst {
+			b.extendData(payload[1+pointerField:])
+		} else {
+			b.length += pointerField
+			b.extendData(payload[1:])
+		}
+	}
+
+	return
+}
+
+func (b *tableScannerBuffer) OK() bool {
+	return b.ok
+}
+
+func (b *tableScannerBuffer) Bytes() []byte {
+	if b.OK() {
+		return b.data[b.length:]
+	}
+
+	return nil
+}
+
+type TableScanner struct {
+	s       PacketStream
+	pid     PID
+	buffers map[PID]*tableScannerBuffer
+}
+
+func NewTableScanner(s PacketStream) *TableScanner {
+	buffers := make(map[PID]*tableScannerBuffer)
+	return &TableScanner{
+		s:       s,
+		buffers: buffers,
+	}
+}
+
+func (f *TableScanner) Scan() bool {
+	for f.s.Scan() {
+		packet := f.s.Packet()
+
+		buffer, ok := f.buffers[packet.PID()]
+		if !ok {
+			f.buffers[packet.PID()] = new(tableScannerBuffer)
+			buffer = f.buffers[packet.PID()]
+		}
+
+		buffer.Extend(packet)
+		if buffer.OK() {
+			f.pid = packet.PID()
+			return true
+		}
+	}
+
+	return false
+}
+
+func (f *TableScanner) Bytes() []byte {
+	buffer := f.buffers[f.pid]
+	if buffer.OK() {
+		return buffer.Bytes()
+	}
+
+	return nil
+}
+
+func (f *TableScanner) Table() Table {
+	return Table(f.Bytes())
 }
