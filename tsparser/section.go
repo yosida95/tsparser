eea19cfc3f43eb7895756c42178dfd4981fc0651
yosida95 <kohei@yosida95.com>
2014-08-14 23:36:40 +0900

    Program Association Section

---
+++ tsparser/section.go
@@ -0,0 +1,31 @@
+package tsparser
+
+type ProgramAssociationSection struct {
+	network    PID
+	programMap map[uint16]PID
+}
+
+func ParseProgramAssociationSection(table Table) *ProgramAssociationSection {
+	sec := new(ProgramAssociationSection)
+	sec.programMap = make(map[uint16]PID)
+
+	payload := table.Data()
+	for i := 0; i < len(payload); i += 4 {
+		programNumber := uint16(payload[i])<<8 | uint16(payload[i+1])
+		if programNumber == 0 {
+			sec.network = PID(payload[i+2]&0x1f)<<8 | PID(payload[i+3])
+		} else {
+			sec.programMap[programNumber] = PID(payload[i+2]&0x1f)<<8 | PID(payload[i+3])
+		}
+	}
+
+	return sec
+}
+
+func (s *ProgramAssociationSection) NetworkPID() PID {
+	return s.network
+}
+
+func (s *ProgramAssociationSection) ProgramMap() map[uint16]PID {
+	return s.programMap
+}
