a2f0ca8debb3d4a58b6b86696b748af3d9e84153
yosida95 <kohei@yosida95.com>
2014-08-16 05:08:09 +0900

    remove `Bytes` method

--- tsparser/stream.go
+++ tsparser/stream.go
@@ -2,12 +2,10 @@ package tsparser
 
 type PacketStream interface {
 	Scan() bool
-	Bytes() []byte
 	Packet() Packet
 }
 
 type TableStream interface {
 	Scan() bool
 	Table() Table
-	Bytes() []byte
 }
