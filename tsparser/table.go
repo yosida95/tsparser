8005af4e099531250025c3c09814c503feedff6b
yosida95 <kohei@yosida95.com>
2014-08-18 02:59:57 +0900

    license under the 3-Clause BSD License

--- tsparser/table.go
+++ tsparser/table.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2014 Kohei YOSHIDA. All rights reserved.
+// This software is licensed under the 3-Clause BSD License
+// that can be found in LICENSE file.
+
 package tsparser
 
 import (
